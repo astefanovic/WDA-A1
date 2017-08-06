c855881473e5e31460232d6669c555d1de03f5d5
astefanovic <astefanovic101@gmail.com>
2017-08-06 20:07:30 +1000

    Added basic form on ticketing page and footer

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -3,11 +3,23 @@
 <head>
     <title> @yield('title') </title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
 </head>
 <body>
 @include('shared.navbar')
 @yield('content')
+<footer>
+    <div class="container">
+        <hr>
+        <div class="row">
+            <div class="col-xs-12">Copyright © 2017 RMIT University |
+                <a href="http://www.rmit.edu.au/utilities/disclaimer/" target="_blank">Disclaimer</a> |
+                <a href="http://www.rmit.edu.au/utilities/privacy/" target="_blank">Privacy</a> |
+                <a href="http://www.rmit.edu.au/utilities/website-feedback/" target="_blank">Website feedback</a> |
+                ABN 49 781 030 034 | CRICOS provider number: 00122A
+            </div>
+        </div>
+    </div>
+</footer>
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
--- src/resources/views/ticket.blade.php
+++ src/resources/views/ticket.blade.php
@@ -2,8 +2,53 @@
 @section('title', 'RMIT Ticketing')
 @section('content')
 <div class="container">
-<div class="jumbotron">
-    <h1>Ticketing page</h1>
-</div>
+    <div class="page-header">
+        <h1>Create ticket</h1>
+    </div>
+
+    <!-- TODO: On button click enable :invalid css selector, alongside php validation -->
+    <div>
+        {!! Form::open(array('url' => '#')) !!}
+        <div class="row">
+            <div class="col-md-4 col-xs-12">
+                <div class="form-group">
+                    {!! Form::email('email', '', ['placeholder' => 'Email', 'class' => 'form-control', 'required' => 'required']) !!}
+                </div>
+            </div>
+            <div class="col-md-4 col-xs-6">
+                <div class="form-group">
+                    {!! Form::text('firstname', '', ['placeholder' => 'Firstname', 'class' => 'form-control', 'required' => 'required']) !!}
+                </div>
+            </div>
+            <div class="col-md-4 col-xs-6">
+                <div class="form-group">
+                    {!! Form::text('lastname', '', ['placeholder' => 'Lastname', 'class' => 'form-control', 'required' => 'required']) !!}
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-6 col-xs-12">
+                <div class="form-group">
+                    {!! Form::text('subject', '', ['placeholder' => 'Subject', 'class' => 'form-control', 'required' => 'required']) !!}
+                </div>
+            </div>
+            <div class="col-md-6 col-xs-12">
+                <div class="form-group">
+                    {!! Form::select('type', ['its' => 'IT Services', 'web' => 'Web Services', 'business' => 'Business Systems', 'arg' => 'ARG'], null, ['placeholder' => 'Issue Type', 'class' => 'form-control', 'required' => 'required']) !!}
+                </div>
+            </div>
+            <div class="col-xs-12">
+                <div class="form-group">
+                    {!! Form::textarea('description', '', ['placeholder' => 'Description', 'class' => 'form-control', 'required' => 'required']) !!}
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            {!! Form::submit(null, ['class' => 'btn btn-primary']) !!}
+        </div>
+
+        {!!  Form::close() !!}
+    </div>
 </div>
 @endsection

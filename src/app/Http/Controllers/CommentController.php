1c42faaf4cac0ca4743c13e4b3cc09b60c4b368a
astefanovic <astefanovic101@gmail.com>
2017-08-14 15:57:29 +1000

    Readded user variable in controller

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -12,8 +12,9 @@ class CommentController extends Controller
     public function store(Request $request) {
         $allRequest = $request->all();
         //Creates a comment based on the email of the panel it was in
-        //and the ticket id, found from the loop on creation in the view
-        Log::info('USERID '.$user->id);
+        //and the ticket id, found from the loop on creation in the
+        //blade file
+        $user = User::where('email', '=', $allRequest['email'])->first();
         $newComment = Comment::create(['text' => $allRequest['text'],
             'user_id' => $user->id,
             'ticket_id' => $allRequest['ticketId']]);

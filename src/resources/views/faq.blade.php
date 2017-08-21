60ce2064f416f366e655d3349e76de16007ee68f
Your Name <you@example.com>
2017-08-21 23:05:11 +1000

    updatedFAQ

--- src/resources/views/faq.blade.php
+++ src/resources/views/faq.blade.php
@@ -4,63 +4,76 @@
     <div class="container">
         <h1 class="display-4 py-5">FAQ</h1>
         <div class="container">
-            <div id="accordion" role="tablist" aria-multiselectable="true">
-                <div class="card">
-                    <div class="card-header" role="tab" id="headingOne">
-                        <h5 class="mb-0">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                How long can I expect until I receive a response?
+            <div id="accordion" class="panel-group">
+                <div class="panel panel-primary">
+                    <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" data-target="#question1">
+                        <h4 class="panel-title">
+                            <a href="#" class="in">
+                                Q: How long can I expect until I receive a response?
                             </a>
-                        </h5>
+                        </h4>
                     </div>
-
-                    <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne">
-                        <div class="card-block">
+                    <div id="question1" class="panel-collapse collapse">
+                        <div class="panel-body" style="padding-top: 10px; padding-bottom: 15px;">
                             Your ticket will be viewed upon the same day as submission but depending on the solution for that task,
                             the waiting day may be up to 5 business days.
                         </div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card-header" role="tab" id="headingTwo">
-                        <h5 class="mb-0">
-                            <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                Do I need to be a student to use the ticketing system?
+                <div class="panel panel-primary">
+                    <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" data-target="#question2">
+                        <h4 class="panel-title">
+                            <a href="#" class="in">
+                                Q: Do I need to be a student to use the ticketing system?
                             </a>
-                        </h5>
+                        </h4>
                     </div>
-                    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
-                        <div class="card-block">
+                    <div id="question2" class="panel-collapse collapse">
+                        <div class="panel-body" style="padding-top: 10px; padding-bottom: 15px;">
                             No. Anyone can use the RMIT ticketing system. If you login as a student the personal details aspect of the form will be
                             automatically filled.
                         </div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card-header" role="tab" id="headingThree">
-                        <h5 class="mb-0">
-                            <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                What if my problem does not fit into any of the selections?
+                <div class="panel panel-primary">
+                    <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" data-target="#question3">
+                        <h4 class="panel-title">
+                            <a href="#" class="in">
+                                Q: What if my problem does not fit into any of the selections?
                             </a>
-                        </h5>
+                        </h4>
                     </div>
-                    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
-                        <div class="card-block">
-                            Well, too bad.
+                    <div id="question3" class="panel-collapse collapse">
+                        <div class="panel-body" style="padding-top: 10px; padding-bottom: 15px;">
+                            ...
                         </div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card-header" role="tab" id="headingFour">
-                        <h5 class="mb-0">
-                            <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                Can I talk in person instead of submitting a ticket online?
+                <div class="panel panel-primary">
+                    <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" data-target="#question4">
+                        <h4 class="panel-title">
+                            <a href="#" class="in">
+                                Q: Can I talk to an individual instead of submitting a ticket online?
                             </a>
-                        </h5>
+                        </h4>
                     </div>
-                    <div id="collapseFour" class="collapse" role="tabpanel" aria-labelledby="headingFour">
-                        <div class="card-block">
-                            Yes. If you prefer to talk to a person please visit the IT service desk in building 80, level 2. Opening hours 9am-5pm.
+                    <div id="question4" class="panel-collapse collapse">
+                        <div class="panel-body" style="padding-top: 10px; padding-bottom: 15px;">
+                            Yes. If you prefer to talk to a staff in person please visit the IT service desk in building 80, level 2. Opening hours 9am-5pm.
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" data-target="#question5">
+                        <h4 class="panel-title">
+                            <a href="#" class="in">
+                                Q: If i accidentally remove a ticket that is pending can i undo it?
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="question5" class="panel-collapse collapse">
+                        <div class="panel-body" style="padding-top: 10px; padding-bottom: 15px;">
+                            No. Unfortunately if you withdraw a request it is deleted off the database therefore you will need to resubmit a request.
                         </div>
                     </div>
                 </div>

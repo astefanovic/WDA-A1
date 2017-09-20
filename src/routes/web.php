fb72822b9e2c74c42cd27de64f669947fcd7598c
astefanovic <astefanovic101@gmail.com>
2017-09-20 21:31:11 +1000

    Added faq method to controller

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,13 +11,13 @@
 |
 */
 
+//Runs the ticket creation and viewing through authentication midddleware
+//to guard from unauthorised access
 Route::get('/', 'PagesController@ticket')->middleware('auth');
 
 Route::get('view', 'PagesController@view')->name('view')->middleware('auth');
 
-Route::get('FAQ', function(){
-    return view('faq');
-});
+Route::get('FAQ', 'PagesController@faq');
 
 Route::post('view', 'TicketController@store');
 
